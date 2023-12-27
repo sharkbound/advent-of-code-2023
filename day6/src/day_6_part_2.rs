721f22763cba50b9e04e13a2c0e7a24fc5ffd559
sharkbound <laptopblaster@gmail.com>
2023-12-26 19:18:53 -0600

    day 2 part 2 solved

--- day6/src/day_6_part_2.rs
+++ day6/src/day_6_part_2.rs
@@ -8,7 +8,7 @@ use nom::sequence::tuple;
 use daytemplate::{Day, DayPart};
 use rustutils::nom_helpers::consume_empty_space;
 
-type NumberType = u64;
+type NumberType = i64;
 
 pub struct Day6Part2 {}
 
@@ -26,7 +26,7 @@ impl Day for Day6Part2 {
     fn parse(&self, input: &str) -> Self::ParseOutput {
         let lines = input.lines().collect::<Vec<_>>();
         let time = lines[0][lines[0].find(':').unwrap() + 1..].replace(" ", "").parse::<NumberType>().unwrap();
-        let distance = lines[1][lines[1].find(':').unwrap() + 1..].replace(" " , "").parse::<NumberType>().unwrap();
+        let distance = lines[1][lines[1].find(':').unwrap() + 1..].replace(" ", "").parse::<NumberType>().unwrap();
         RaceRecord { time, distance }
     }
 
@@ -34,7 +34,23 @@ impl Day for Day6Part2 {
         // let input = self.sample("part_1");
         let input = self.input();
         let parsed = self.parse(&input);
-        println!("Day 6 Part 2: {:?}", parsed);
+        let center = parsed.time / 2;
+
+        let mut ways_to_win = 0;
+        for hold_time in DirectionalNumberRange::new(center - 1, -1) {
+            if hold_button(&parsed, hold_time) > parsed.distance {
+                ways_to_win += 1;
+            } else { break; }
+        }
+        for hold_time in DirectionalNumberRange::new(center + 1, 1) {
+            if hold_button(&parsed, hold_time) > parsed.distance {
+                ways_to_win += 1;
+            } else { break; }
+        }
+        if hold_button(&parsed, center) > parsed.distance {
+            ways_to_win += 1;
+        }
+        println!("Day 6 Part 2: {:?}", ways_to_win);
     }
 }
 
@@ -43,6 +59,38 @@ fn hold_button(record: &RaceRecord, hold_time: NumberType) -> NumberType {
         return 0;
     }
     (record.time - hold_time) * hold_time
+}
+
+
+struct DirectionalNumberRange {
+    current: NumberType,
+    started: bool,
+    direction: NumberType,
+}
+
+impl DirectionalNumberRange {
+    fn new(current: NumberType, direction: NumberType) -> DirectionalNumberRange {
+        Self {
+            current,
+            started: false,
+            direction,
+        }
+    }
+}
+
+impl Iterator for DirectionalNumberRange {
+    type Item = NumberType;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if !self.started {
+            self.started = true;
+            Some(self.current)
+        } else {
+            let ret = self.current + self.direction;
+            self.current = ret;
+            Some(ret)
+        }
+    }
 }
 
 #[derive(Debug)]
